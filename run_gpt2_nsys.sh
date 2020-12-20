64d17d7c7474d0a9c4b2a89952e4f3b2985ed8d2
kooyunmo <mpbb03@naver.com>
2020-12-21 00:30:29 +0900

    nsys script

--- run_gpt2_nsys.sh
+++ run_gpt2_nsys.sh
@@ -1,5 +1,5 @@
 nsys profile \
   --trace=cublas,cuda,cudnn,nvtx,osrt \
-  -o ./nsys/test_small.output \
+  -o ./nsys/gpt3_6.7b.output \
   -f true \
-  python run_gpt2.py
+  python run_gpt2.py --model gpt3_6.7b --enable-prefetch --warmups 0
